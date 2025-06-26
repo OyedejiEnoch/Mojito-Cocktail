6518eac0b3dc524a434f2d3572e4b3bd7ec0bd10
Enoch <oyedejienoch@gmail.com>
2025-06-26 14:08:54 +0100

    first push

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import gsap from 'gsap'
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function App() {
+
+
+  return (
+    <div className='flex-center h-[100vh]'>
+        <h1 className='text-indigo-500 text-3xl'>Hello, Gsap</h1>
+    </div>
+  )
+}
+
+export default App
